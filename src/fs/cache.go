9cd600f706b20b133f55cd12de9b30ef81a1f9e0
Vishwas Mittal <vishwas.mittal78@gmail.com>
2019-03-23 23:38:37 +0530

    changes in thumbnail creation and saving process

--- src/fs/cache.go
+++ src/fs/cache.go
@@ -8,15 +8,21 @@ import (
 )
 
 func thumbnailer(imagePath string, savePath string) error {
-	//// source https://www.socketloop.com/tutorials/golang-generate-thumbnails-from-images
-	log("path: ", imagePath)
 	img, err := imaging.Open(imagePath)
 	if err != nil {
 		log("some error occurred")
 		return err
 	}
-	thumb := imaging.Thumbnail(img, 100, 100, imaging.CatmullRom)
+	log("path: ", imagePath)
+	imgX := img.Bounds().Max.X
+	imgY := img.Bounds().Max.Y
 
+	thumbX := (imgX * 100 * 2) / (imgX + imgY)
+	thumbY := (imgY * 100 * 2) / (imgX + imgY)
+
+	thumb := imaging.Thumbnail(img, thumbX, thumbY, imaging.NearestNeighbor)
+
+	os.Mkdir(filepath.Dir(savePath), os.ModePerm)
 	err = imaging.Save(thumb, savePath)
 	if err != nil {
 		return err
@@ -26,46 +32,27 @@ func thumbnailer(imagePath string, savePath string) error {
 }
 
 func fillCache(root string) error {
-	filepath.Walk(root, getWalkFunction())
+	filepath.Walk(root, walkFunc)
 	return nil
 }
 
-func getWalkFunction() func(path string, info os.FileInfo, err error) error {
-	return func(path string, info os.FileInfo, err error) error {
-		if strings.Contains(path, ".fscache") {
-			return nil
-		}
-		fi, _ := os.Stat(path)
-		// if the current path is to directory, check if the .fscache/thumbnail subdirectories exists.
-		if fi.IsDir() {
-			// if the current path is directory, check for existence of .fscache and thumbnails
-			thumbnailDirPath := filepath.Join(path, ".fscache/thumbnails")
-			_, err := os.Stat(filepath.Join(thumbnailDirPath))
-			if os.IsNotExist(err) {
-				// if thumbnail directory doesn't exist, create it.
-				os.MkdirAll(thumbnailDirPath, os.ModePerm)
-			}
-		} else {
-			// if the path is to a file instead of directory
-			parentDir := filepath.Dir(path)
-			filename := filepath.Base(path)
-
-			// this directory should exist because we are walking
-			// down the tree and making .fscache for each directory
-			// EDIT: this is not necessary. Human intervention can change
-			// the directories any time.
-			thumbnailDirPath := filepath.Join(parentDir, ".fscache/thumbnails")
-			_, err := os.Stat(thumbnailDirPath)
-			if os.IsNotExist(err) {
-				os.MkdirAll(thumbnailDirPath, os.ModePerm)
-			}
-			imageInfo, _ := os.Stat(path)
-			thumbnailPath := filepath.Join(thumbnailDirPath, filename)
-			thumbnailInfo, err := os.Stat(thumbnailPath)
-			if os.IsNotExist(err) || imageInfo.ModTime().After(thumbnailInfo.ModTime()) {
-				thumbnailer(path, thumbnailPath)
-			}
-		}
+func walkFunc(path string, info os.FileInfo, err error) error {
+	if strings.Contains(path, ".fscache") {
 		return nil
 	}
+	fi, _ := os.Stat(path)
+	if ! fi.IsDir() {
+		// if the path is to a file instead of directory
+		parentDir := filepath.Dir(path)
+		filename := filepath.Base(path)
+
+		thumbnailDirPath := filepath.Join(parentDir, ".fscache/thumbnails")
+		thumbnailPath := filepath.Join(thumbnailDirPath, filename)
+		imageInfo, _ := os.Stat(path)
+		thumbnailInfo, err := os.Stat(thumbnailPath)
+		if os.IsNotExist(err) || imageInfo.ModTime().After(thumbnailInfo.ModTime()) {
+			thumbnailer(path, thumbnailPath)
+		}
+	}
+	return nil
 }
